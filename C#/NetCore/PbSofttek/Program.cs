fa63bf1835c6a7b821ce7b4ed96cc9cc97e265fc
jos-gabriel <jos.gabrie@gmail.com>
2023-08-15 03:01:36 -0600

    refactor(C#-PbSofttek): Par o Impar

--- C#/NetCore/PbSofttek/Program.cs
+++ C#/NetCore/PbSofttek/Program.cs
@@ -1,11 +1,44 @@
-﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
-int i = 0;
-int MaxElement = array.Length;
-
-Console.WriteLine("Número de elementos en el arreglo " + MaxElement + "\n");
-
-while (i < MaxElement)
+﻿internal class Program
 {
-    Console.Write(array[i] + " ");
-    i++;
+    private static void Main(string[] args)
+    {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        int i = 0;
+        int MaxElement = array.Length;
+
+        while (i < MaxElement)
+        {
+
+            EsPar(array, i);
+            EsImpar(array, i);
+            i++;
+        }
+
+    }
+
+    public static void EsPar(int[] array, int i)
+    {
+        int divisor = 2;
+
+        int dividendo = array[i];
+
+        if (dividendo%divisor == 0)
+        {
+            Console.WriteLine(array[i] + " es par");
+        }
+
+    }
+
+    public static void EsImpar(int[] array, int i)
+    {
+        int divisor = 2;
+
+        int dividendo = array[i];
+
+        if (dividendo % divisor != 0)
+        {
+            Console.WriteLine(array[i] + " es impar");
+        }
+
+    }
 }
