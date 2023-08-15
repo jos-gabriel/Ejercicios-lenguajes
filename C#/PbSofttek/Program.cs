098bd4ad82cdbfd84020be4926fd5c562ed5e616
jos-gabriel <jos.gabrie@gmail.com>
2023-08-15 00:32:52 -0600

    feat(C#-PbSoftek): Recorrer un Array con While

--- C#/PbSofttek/Program.cs
+++ C#/PbSofttek/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+int i = 0;
+int MaxElement = array.Length;
+
+Console.WriteLine("Número de elementos en el arreglo " + MaxElement + "\n");
+
+while (i < MaxElement)
+{
+    Console.Write(array[i] + " ");
+    i++;
+}
