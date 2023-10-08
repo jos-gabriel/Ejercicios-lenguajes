6f943325168635c0d6ccb4828ec095ce8d7b5f51
jos-gabriel <jos.gabrie@gmail.com>
2023-10-07 18:22:02 -0600

    refactor(TypeScript)funciones objetos como argumentos

--- JavaScript/TypeScript/Angular/01-TypeScript/src/topics/03-functions.ts
+++ JavaScript/TypeScript/Angular/01-TypeScript/src/topics/03-functions.ts
@@ -14,16 +14,33 @@ const result: number = addNumbers(1,2);
 const result2: string = addNumbersArrow(1,2);
 const multiplyResult: number = multiply(8);
 
-
+interface Character {
+    name: string;
+    hp: number;
+    showHP: () => void;
+}
 
 console.log({result,result2, multiplyResult});
 console.log(result,result2, multiplyResult);
 
+const healCharacter = (character:Character, amount: number) => {
+    character.hp += amount;
+}
 
 
+const strider: Character = {
+    name: "Strider",
+    hp: 50,
+    showHP: function () {
+        console.log(`Puntos de vida: ${this.hp}`);
+    },
+};
+
+healCharacter (strider, 10);
+healCharacter (strider, 50);
 
 
-
+strider.showHP();
 
 
 
